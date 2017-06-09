4d7cf3aee6c907fa5bfc8a0e5e5c02096ba2c7be
Daniel Kluss <codepilot@gmail.com>
2017-06-08 20:25:03 -0700

    Fixed timing, wait for Dom Content to Load

--- css-page.js
+++ css-page.js
@@ -59,53 +59,57 @@ function groupParseSelector(selectorText) {
 //console.log(groupParseSelector('body>div.test1>span.redText'));
 
 //    return;
-
-Array.prototype.forEach.call(document.styleSheets, (styleSheet)=> {
-    Array.prototype.forEach.call(styleSheet.cssRules, (cssRule)=> {
-        let previousSelector;
-        groupParseSelector(cssRule.selectorText).forEach((selector, si, parsed) => {
-            const partialArray = parsed.slice(0, si + 1);
-            const partial = parsed.slice(0, si + 1).join('');
-            try {
-                let selected = document.querySelector(partial);
-                if(selected === null) {
-                    //console.log('need to create', selector, 'in', previousSelector);
-                    if(selector instanceof Elemental) {
-                        //console.log('elemental selector', selector);
-                        let newElement;
-                        selector.args.forEach((elementalPart) => {
-                            switch(true) {
-                                case elementalPart instanceof TagSelector:
-                                    newElement = document.createElement(`${elementalPart}`);
-                                    selected = newElement;
-                                    break;
-                                case elementalPart instanceof ClassSelector:
-                                    newElement.classList.add(elementalPart.slice(1));
-                                    break;
-                                case elementalPart instanceof AttributeEqualsSelector:
-                                    newElement.setAttribute(elementalPart.executed[1], elementalPart.executed[2]);
-                                    break;
-                                default:
-                                    console.log('unhandled', elementalPart);
-                            }
-                        });
-                        //console.log('newElement', newElement);
-                        selected = previousSelector.appendChild(newElement);
+function CssToHtml() {
+    console.log('loaded');
+    Array.prototype.forEach.call(document.styleSheets, (styleSheet)=> {
+        Array.prototype.forEach.call(styleSheet.cssRules, (cssRule)=> {
+            let previousSelector;
+            groupParseSelector(cssRule.selectorText).forEach((selector, si, parsed) => {
+                const partialArray = parsed.slice(0, si + 1);
+                const partial = parsed.slice(0, si + 1).join('');
+                try {
+                    let selected = document.querySelector(partial);
+                    if(selected === null) {
+                        //console.log('need to create', selector, 'in', previousSelector);
+                        if(selector instanceof Elemental) {
+                            //console.log('elemental selector', selector);
+                            let newElement;
+                            selector.args.forEach((elementalPart) => {
+                                switch(true) {
+                                    case elementalPart instanceof TagSelector:
+                                        newElement = document.createElement(`${elementalPart}`);
+                                        selected = newElement;
+                                        break;
+                                    case elementalPart instanceof ClassSelector:
+                                        newElement.classList.add(elementalPart.slice(1));
+                                        break;
+                                    case elementalPart instanceof AttributeEqualsSelector:
+                                        newElement.setAttribute(elementalPart.executed[1], elementalPart.executed[2]);
+                                        break;
+                                    default:
+                                        console.log('unhandled', elementalPart);
+                                }
+                            });
+                            //console.log('newElement', newElement);
+                            selected = previousSelector.appendChild(newElement);
+                        }
+                    } else {
+                        //console.log({selector, partialArray, partial, selected});                        
                     }
-                } else {
-                    //console.log({selector, partialArray, partial, selected});                        
+                    previousSelector = selected;
+                } catch( err ) {
                 }
-                previousSelector = selected;
-            } catch( err ) {
-            }
-        });
-        try {
-            if(cssRule.style.content.length) {
-                //console.log('content', cssRule.style.content, previousSelector, cssRule.style);
-                previousSelector.innerHTML = JSON.parse(cssRule.style.content);
-            }
-        } catch(err) {
+            });
+            try {
+                if(cssRule.style.content.length) {
+                    //console.log('content', cssRule.style.content, previousSelector, cssRule.style);
+                    previousSelector.innerHTML = JSON.parse(cssRule.style.content);
+                }
+            } catch(err) {
 
-        }
-    })
-});
+            }
+        })
+    });
+}
+
+document.addEventListener("DOMContentLoaded", CssToHtml);
